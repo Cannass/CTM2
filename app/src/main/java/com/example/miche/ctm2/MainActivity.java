90b7052f2f19096407b78baeb9a3a8177214bc97
miche <michele.cannass@gmail.com>
2018-11-11 17:43:40 +0100

    Suddivisione funzioni nella classe Orari

--- app/src/main/java/com/example/miche/ctm2/MainActivity.java
+++ app/src/main/java/com/example/miche/ctm2/MainActivity.java
@@ -37,93 +37,17 @@ public class MainActivity extends AppCompatActivity {
         final TextView nome_palina = (TextView) findViewById(R.id.nome_palina);
         final ListView item = (ListView) findViewById(R.id.list_item);
 
-        try {
-            // get URL content
-
-
-
             if (Build.VERSION.SDK_INT > 9)
             {
                 StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
                 StrictMode.setThreadPolicy(policy);
             }
-
-            Document doc = Jsoup.connect("http://www.ctmcagliari.it/linee_orari.php?linea=01&verso=Di&palina=PA0205").timeout(10000).get();
-//            Elements newsHeadlines = doc.select("#mp-itn b a");
-//            String a="http://www.ctmcagliari.it/linee_orari.php?linea=01&verso=Di&palina=PA0205";
-//            Connection conn = new  Connection();
-
-            //Element masthead = doc.select("div[id*=tempo_reale").first();
-            //StringBuilder builder= conn.doInBackground(a);
-            try {
-                Element mast = doc.getElementById("tempo_reale");
-                Elements tbody = mast.getElementsByTag("tbody");
-                Elements ora1 = mast.getElementsByTag("tr");
-                List<String> Stringhe = new ArrayList<String>();
-                for(int i =0;i<=ora1.size()-1;i++){
-                    if(i != 0){
-                        Element orarioprogrammato = ora1.get(i);
-                        Element oraprogrammata = orarioprogrammato.getElementsByClass("ore").first();
-                        Elements minutiprogrammati = orarioprogrammato.getElementsByIndexEquals(1);
-                        String minuti[] = minutiprogrammati.get(0).childNodes().get(0).toString().split(" ");
-                        String val = "";
-                        for(int j=0; j<=minuti.length -1; j++){
-                                val = oraprogrammata.childNodes().get(0).toString() + ":" + minuti[j];
-                                Stringhe.add(val);
-                        }
-                    }
-                }
-
-                nome_palina.setText(doc.getElementById("nome_palina").childNode(0).toString());
-                final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
-                        (this, android.R.layout.simple_list_item_1, Stringhe);
-                item.setAdapter(arrayAdapter);
-
-            } catch (Exception e) {
-                Element orari = doc.getElementsByClass("tabella_orari testo").first();
-                Elements ora = orari.getElementsByTag("tr");
-                Element firstorario = ora.get(1);
-                String val = "";
-                String minuti[] =firstorario.childNode(3).childNode(0).toString().split(" ");
-                for(int i = 0; i<=minuti.length -1;i++){
-                    if(val == ""){
-                        val = firstorario.getElementsByClass("ore").first().text() + ":" + minuti[i];
-                    }
-                    else{
-                        val = val + "\n" +firstorario.getElementsByClass("ore").first().text() + ":" + minuti[i];
-                    }
-                }
-//                text2.setText( val);
-
-            }
-
-
-
-//            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
-//            Notification n  = new Notification.Builder(this)
-//                    .setContentTitle("New mail from " + "test@gmail.com")
-//                    .setContentText(ore.text())
-//                    .setSmallIcon(R.drawable.ic_launcher_background)
-//                    .setAutoCancel(true).build();
-//
-//            NotificationManager notificationManager =
-//                    (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-//            notificationManager.notify(0, n);
-
-
-//            synchronized(mBuilder){
-//                mBuilder.notify();
-//            }
-
-
-
-
-            String prova = "";
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+            String url = "http://www.ctmcagliari.it/linee_orari.php?linea=01&verso=Di&palina=PA0205";
+            Orari or = new Orari(url,10000);
+            List<String> Stringhe = or.GetOrariProgrammati();
+            final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                    (this, android.R.layout.simple_list_item_1, Stringhe);
+            nome_palina.setText(or.GetPalinaName());
+            item.setAdapter(arrayAdapter);
     }
 }
--- app/src/main/java/com/example/miche/ctm2/Orari.java
+++ app/src/main/java/com/example/miche/ctm2/Orari.java
@@ -2,25 +2,61 @@ package com.example.miche.ctm2;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Orari {
 
-    public  static  GetOrariProgrammati(String url,int timeout){
-        try {
-            Document doc = Jsoup.connect(url).timeout(timeout).get();
-
-
-
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
+    private Document doc = null;
+    //Costruttore
+    public Orari(String url,int timeout){
+        this.doc = GetDocFromUrl(url,timeout);
     }
 
+    public List<String> GetOrariProgrammati(){
+        List<String> Stringhe = new ArrayList<String>();
+        if(this.doc== null){return Stringhe;}
+        try{
+            Element tempo_reale = this.doc.getElementById("tempo_reale");
+            Elements tbody = tempo_reale.getElementsByTag("tbody");
+            Elements orari = tempo_reale.getElementsByTag("tr");
+            for(int i =0;i<=orari.size()-1;i++){
+                if(i != 0){
+                    Element orarioprogrammato = orari.get(i);
+                    Element oraprogrammata = orarioprogrammato.getElementsByClass("ore").first();
+                    Elements minutiprogrammati = orarioprogrammato.getElementsByIndexEquals(1);
+                    String minuti[] = minutiprogrammati.get(0).childNodes().get(0).toString().split(" ");
+                    String val = "";
+                    for(int j=0; j<=minuti.length -1; j++){
+                        val = oraprogrammata.childNodes().get(0).toString() + ":" + minuti[j];
+                        Stringhe.add(val);
+                    }
+                }
+            }
 
+
+        }catch (Exception e){
+
+        }
+        return Stringhe;
+    }
+
+    public  static  Document GetDocFromUrl(String url,int timeout){
+        try {
+            Document doc = Jsoup.connect(url).timeout(timeout).get();
+            return doc;
+        }catch (Exception e){
+            return null;
+        }
+    }
+    public  String GetPalinaName(){
+        if(this.doc != null ){
+            return this.doc.getElementById("nome_palina").childNode(0).toString();
+        }
+        return "";
+    }
 }
