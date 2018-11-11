968ccec47e205b15aba9b761db612958f86043c0
miche <michele.cannass@gmail.com>
2018-11-11 20:42:21 +0100

    Merda

--- app/src/main/java/com/example/miche/ctm2/Database.java
+++ app/src/main/java/com/example/miche/ctm2/Database.java
@@ -1,7 +1,14 @@
 package com.example.miche.ctm2;
 
 
+import android.content.res.AssetManager;
 import android.database.sqlite.SQLiteDatabase;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import static android.content.Context.MODE_PRIVATE;
 
@@ -12,8 +19,9 @@ public class Database {
     }
 
     public static void Check(SQLiteDatabase Db){
-        Db.execSQL("CREATE TABLE IF NOT EXISTS TutorialsPoint(Fermata VARCHAR,IdFermata VARCHAR);");
-        Db.execSQL("INSERT INTO TutorialsPoint VALUES('admin','admin');");
+        Db.execSQL("CREATE TABLE IF NOT EXISTS Fermate(Fermata VARCHAR,IdFermata VARCHAR);");
+        Db.execSQL("INSERT INTO Fermate VALUES('Pascoli (ang. via Petrarca)','0205');");
+        Db.execSQL("INSERT INTO Fermate VALUES('Sonnino (ang. via Abba)','0020');");
     }
 
 
--- app/src/main/java/com/example/miche/ctm2/SelezioneFermate.java
+++ app/src/main/java/com/example/miche/ctm2/SelezioneFermate.java
@@ -1,6 +1,8 @@
 package com.example.miche.ctm2;
 
 import android.annotation.SuppressLint;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -15,6 +17,15 @@ public class SelezioneFermate extends AppCompatActivity {
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
         final Spinner spinner = (Spinner)findViewById(R.id.spinner2);
+        SQLiteDatabase mydatabase = openOrCreateDatabase("CTMData",MODE_PRIVATE,null);
+        Cursor resultSet = mydatabase.rawQuery("Select Fermata from Fermate",null);
+        while(resultSet.moveToNext()){
+            String username = resultSet.getString(0);
+            resultSet.moveToNext();
+
+        }
+        resultSet.moveToFirst();
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.fermate,android.R.layout.simple_spinner_item);
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
