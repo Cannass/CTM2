b1b4e91a51f820930219fa8c5d2bbf006d3608e4
miche <michele.cannass@gmail.com>
2018-11-18 23:01:57 +0100

    Shit

--- app/src/main/java/com/example/miche/ctm2/MainActivity.java
+++ app/src/main/java/com/example/miche/ctm2/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         TxtReader quoteBank = new TxtReader(getApplicationContext());
-        InputStream is  = quoteBank.mContext.getAssets().open("paline.txt");
+        List<String> lines = quoteBank.readLine("Fermate.txt");
         settings.setVisibility(View.VISIBLE);
         settings.setOnClickListener( new View.OnClickListener() {
             @Override
@@ -44,37 +44,37 @@ public class MainActivity extends AppCompatActivity {
         });
         SQLiteDatabase mydatabase = openOrCreateDatabase("CTMData",MODE_PRIVATE,null);
         Database.Check(mydatabase);
-        int count =Database.CheckPreferite(mydatabase);
-        if(count == 0){
-            Database.CloseConnection(mydatabase);
-            Intent myIntent = new Intent(MainActivity.this, SelezioneFermate.class);
-            MainActivity.this.finish();
-            MainActivity.this.startActivity(myIntent);
-
-        }else{
-            if (Build.VERSION.SDK_INT > 9)
-            {
-                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-                StrictMode.setThreadPolicy(policy);
-            }
-            String fermata = Database.GetPreferita(mydatabase);
-            Database.CloseConnection(mydatabase);
-            String url = "";
-            if(fermata.equals("Pascoli (ang. via Petrarca)")){
-                url = "http://www.ctmcagliari.it/linee_orari.php?linea=01&verso=Di&palina=PA0205";
-            }else if (fermata.equals("Sonnino (ang. via Abba)")){
-                url = "http://www.ctmcagliari.it/linee_orari.php?linea=10&verso=Di&palina=SO0020";
-            }else{
-                url = "http://www.ctmcagliari.it/linee_orari.php?linea=29&verso=As&palina=DA0206";
-            }
-            url = "http://m.ctmcagliari.it/fermate.php?s=0020";
-            Orari or = new Orari(url,10000);
-            List<String> st= or.GetTestOrari();
-            List<String> Stringhe = or.GetOrariProgrammati();
-            final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
-                    (this, android.R.layout.simple_list_item_1, Stringhe);
-            nome_palina.setText(or.GetPalinaName());
-            item.setAdapter(arrayAdapter);
-        }
+        //int count =Database.CheckPreferite(mydatabase);
+//        if(count == 0){
+//            Database.CloseConnection(mydatabase);
+//            Intent myIntent = new Intent(MainActivity.this, SelezioneFermate.class);
+//            MainActivity.this.finish();
+//            MainActivity.this.startActivity(myIntent);
+//
+//        }else{
+//            if (Build.VERSION.SDK_INT > 9)
+//            {
+//                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+//                StrictMode.setThreadPolicy(policy);
+//            }
+//            //String fermata = Database.GetPreferita(mydatabase);
+//            Database.CloseConnection(mydatabase);
+//            String url = "";
+//            if(fermata.equals("Pascoli (ang. via Petrarca)")){
+//                url = "http://www.ctmcagliari.it/linee_orari.php?linea=01&verso=Di&palina=PA0205";
+//            }else if (fermata.equals("Sonnino (ang. via Abba)")){
+//                url = "http://www.ctmcagliari.it/linee_orari.php?linea=10&verso=Di&palina=SO0020";
+//            }else{
+//                url = "http://www.ctmcagliari.it/linee_orari.php?linea=29&verso=As&palina=DA0206";
+//            }
+//            url = "http://m.ctmcagliari.it/fermate.php?s=0020";
+//            Orari or = new Orari(url,10000);
+//            List<String> st= or.GetTestOrari();
+//            List<String> Stringhe = or.GetOrariProgrammati();
+//            final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+//                    (this, android.R.layout.simple_list_item_1, Stringhe);
+//            nome_palina.setText(or.GetPalinaName());
+//            item.setAdapter(arrayAdapter);
+//        }
     }
 }
